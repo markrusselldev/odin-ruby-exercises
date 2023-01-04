8217ef9bd0562722d3f2a48ec36ccaa9f9b7f731
markrusselldev <mark@markrussell.dev>
2023-01-04 15:22:15 -0700

    complete 10_basic_enumerables

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,18 +1,18 @@
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
-  inventory_list.each { |key, value| 
+  inventory_list.each do |key, value| 
     puts "#{key.to_s}, quantity: #{value}"
-  }
+  end
 end
 
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
-  guesses.each_with_index { |guess, idx|
+  guesses.each_with_index do |guess, idx|
     puts "Guess ##{idx + 1} is #{guess}"
-   }
+  end
 end
 
 def find_absolute_values(numbers)
@@ -24,17 +24,15 @@ end
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
-  inventory_list.select { |key, value| 
-    value < 4
-  }
+  inventory_list.select { |key, value| value < 4 }
 end
 
 def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
-  word_list.reduce(Hash.new(0)) { |acc, word|
+  word_list.reduce(Hash.new(0)) do |acc, word|
    acc[word] = word.length
    acc
-  }
+  end
 end
